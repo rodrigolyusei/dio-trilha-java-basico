892b9b16379e0c039729ced0fa38b0fb10f60dd0
JolidPnake <rodrigo0607suguimoto@gmail.com>
2024-05-29 00:56:39 -0300

    feat: funcionamento completo do desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        // Configura o scanner para aceitar ponto como separador decimal
+        Locale.setDefault(Locale.US);
+
+        // Cria um scanner para capturar a entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        // Usar o nextInt retorna o Enter para o proximo next, o que pula a agencia
+        //Para evitar isso, usamos o nextLine e convetemos para inteiro depois
+        System.out.print("Por favor, digite o número da Agência:\t");
+        int numeroAgencia = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Agora me informe a sua Agência:\t\t");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Digite o seu nome completo:\t\t");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.print("E por fim, digite o seu saldo:\t\t");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroAgencia + " e seu saldo " + saldo + " já está disponível para saque");
+
+        // Fecha o scanner
+        scanner.close();
     }
 }
